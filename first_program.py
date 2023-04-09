3edb5dc305e14e629f2d6a2f1f9268b18df65836
sonu simariya <lalitsimariya14@gmail.com>
2023-04-09 17:39:57 +0530

    ok

--- first_program.py
+++ first_program.py
@@ -1,3 +1,4 @@
+# Simple calculater
 a=int(input("enter 1st number"))
 b=int(int(input("enter 2nd number")))
 print("the addition is: ",a+b, 
@@ -5,3 +6,11 @@ print("the addition is: ",a+b,
 "The remaindar is: ",a%b,
 "\nthe multiplication is: ",a*b,
 "\nthe subtraction is: ",a-b)
+
+
+# Using For loop
+j=0
+a="banana appale mango"
+for i in a:
+    print(i,j)
+    j+=1
