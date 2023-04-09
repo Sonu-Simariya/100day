f730b4b74afcf86899b3aa55b02846d086f5f036
sonu simariya <lalitsimariya14@gmail.com>
2023-04-10 01:12:46 +0530

    ok

---
+++ function.py
@@ -0,0 +1,32 @@
+
+# def multiply(a,b):
+#     print("The multiplication is",a*b)
+# a=34
+# b=67
+# multiply(a,b)
+
+
+# a=67
+# b=65
+# multiply(a,b)
+
+
+# def avg(*number):
+#     sum=0
+#     for i in number:
+#         sum=sum+i
+#         print("avg is",sum/len(number))
+# avg(7,5)
+
+# Function
+def cp(p,r,t):
+    # c=p+r+t
+    amount = p*((1+(r/100))**t)
+    ci=amount-p
+    si=(p*r*t)/100
+    print("The amount is: ",amount)
+    print("The compound intreast is: ",ci)
+    print("The simple intreast is: ",si)
+cp(10000,10,2)
+
+cp(10000,10,2)
