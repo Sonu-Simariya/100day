28b7c94003c35511056349345b625cfa1f5c5426
sonu simariya <lalitsimariya14@gmail.com>
2023-04-09 17:07:50 +0530

    go

--- exm.py
+++
@@ -1,5 +0,0 @@
-import seaborn as sns
-print("shvfh")
-a=182
-b=484
-print(a+b)
---
+++ first_program.py
@@ -0,0 +1,9 @@
+a=int(input("enter 1st number"))
+b=int(int(input("enter 2nd number")))
+print("the addition is: ",a+b, 
+"\nthe division is: ",a/b,"\n",
+"The remaindar is: ",a%b,
+"\nthe multiplication is: ",a*b,
+"\nthe subtraction is: ",a-b)
+
+
