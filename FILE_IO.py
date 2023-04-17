0b2d589584865c0aa38ee5af105120d98c0e887e
sonu simariya <lalitsimariya14@gmail.com>
2023-04-17 14:13:47 +0530

    yes

--- FILE_IO.py
+++ FILE_IO.py
@@ -1,4 +1,15 @@
-f=open('requirement.txt','r')
+# to open the file....by default read (r) is here
+f=open('requirment.txt','r')
 read=f.read()
 print(read)
 f.close()
+
+
+#  to add more info in file
+f=open('requirment1.txt','a')
+append=f.write('\nsonusoiamr')
+print(append)
+f.close()
+
+# when file doesn't exist 
+# so python automatically create the name of file
