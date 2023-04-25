17e6c1ce5ab994a56f5c3caf04615413f746bc0d
sonu simariya <lalitsimariya14@gmail.com>
2023-04-25 14:26:23 +0530

    ok

--- arr.py
+++ arr.py
@@ -1,8 +1,10 @@
-a=[]
-z=int(input("enter number of values: "))
-b=0
-for b in range (z):
-    c=int(input("enter value: "))
-    a.append(c)
-a.sort()
-print(a)
+def sonu():
+    a=[]
+    z=int(input("enter number of values: "))
+    b=0
+    for b in range (z):
+        c=int(input("enter value: "))
+        a.append(c)
+    a.sort()
+    print(a)
+sonu()
